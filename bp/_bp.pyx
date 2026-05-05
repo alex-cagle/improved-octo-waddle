03438a866b0de35479f60575c1d79d34a62a2619
alex-cagle <acagle1000@gmail.com>
2026-05-05 10:02:42 -0700

    Compute minselect with running excess

--- bp/_bp.pyx
+++ bp/_bp.pyx
@@ -665,13 +665,30 @@ cdef class BP:
 
     def minselect(self, SIZE_t i, SIZE_t j, SIZE_t q):
         """position of the qth minimum in excess(i), excess(i + 1), . . . , excess(j)."""
-        counts = np.array([self.excess(k) for k in range(i, j + 1)])
-        index = counts == counts.min()
+        cdef SIZE_t pos
+        cdef SIZE_t min_pos
+        cdef SIZE_t count
+        cdef int min_v
+        cdef int excess
 
-        if index.sum() < q:
-            return None
-        else:
-            return i + index.nonzero()[0][q - 1]
+        min_pos = self.rmq(i, j)
+        min_v = _excess_from_block_seed(self, min_pos)
+        excess = _excess_from_block_seed(self, i)
+        count = 0
+
+        if excess == min_v:
+            count += 1
+            if count == q:
+                return i
+
+        for pos in range(i + 1, j + 1):
+            excess += -1 + (2 * self._b_ptr[pos])
+            if excess == min_v:
+                count += 1
+                if count == q:
+                    return pos
+
+        return None
 
     cpdef SIZE_t nsibling(self, SIZE_t i) nogil:
         """The next sibling of i (i.e., the sibling to the right)
